f3514db7d79502127ec96ac0d7262c366df6f0e3
Lego-RL <joeylbarroso@gmail.com>
2022-06-20 19:20:44 -0500

    new top artist and scrobbles commands

--- bot/lfm.py
+++ bot/lfm.py
@@ -1,12 +1,23 @@
+import string
 import discord
-from discord.commands import slash_command
+from discord import ApplicationContext, Embed
+from discord.commands import slash_command, SlashCommandGroup, option
 from discord.ext import commands
 
 import pylast
 
 from main import LFM_API_KEY, LFM_API_SECRET, LFM_USER, LFM_PASS
 
-guilds = [315782312476409867]
+guilds = [315782312476409867, 938179110558105672]
+
+PERIODS = {
+    "7 days": pylast.PERIOD_7DAYS,
+    "1 month": pylast.PERIOD_1MONTH,
+    "3 months": pylast.PERIOD_3MONTHS,
+    "6 months": pylast.PERIOD_6MONTHS,
+    "12 months": pylast.PERIOD_12MONTHS,
+    "overall": pylast.PERIOD_OVERALL,
+}
 
 
 class LastFM(commands.Cog):
@@ -18,6 +29,24 @@ class LastFM(commands.Cog):
             api_secret=LFM_API_SECRET,
             username=LFM_USER,
             password_hash=LFM_PASS,
+        )
+
+    top = SlashCommandGroup(
+        "top",
+        "See info on your top artists, tracks, etc.",
+        guilds=guilds,
+        guild_ids=guilds,
+    )
+
+    @slash_command(name="scrobbles", guilds=guilds)
+    async def scrobbles(self, ctx, name: str = "Lego_RL"):
+        """
+        Display how many total scrobbles the user has.
+        """
+
+        user = self.network.get_user(name)
+        await ctx.respond(
+            f"{user.get_name()} has **{user.get_playcount()}** total scrobbles!"
         )
 
     @slash_command(name="now", guilds=guilds)
@@ -66,6 +95,10 @@ class LastFM(commands.Cog):
             songs.reverse()  # search through least listened tracks first
 
             for song, _ in songs:
+
+                if "video" in str(song).lower():
+                    continue
+
                 if not include_remixes and "remix" in str(song).lower():
                     continue
 
@@ -78,6 +111,47 @@ class LastFM(commands.Cog):
             rtn += f"\n{i+1}) {song.get_name()} by {song.get_artist()}"
 
         await ctx.respond(f"Here are your suggestions:\n{rtn}")
+
+    @top.command(name="artists", description="See a list of your top ten artists.")
+    @option(
+        name="period",
+        type=str,
+        description="Decides the period of time to find your top artists for",
+        choices=["7 days", "1 month", "3 months", "6 months", "12 months", "overall"],
+        required=False,
+        default="overall",
+    )
+    async def top_artists(
+        self,
+        ctx: ApplicationContext,
+        name: str = "Lego_RL",
+        period: str = "overall",
+    ) -> None:
+        """
+        Display the user's top 10 artists, and how many scrobbles
+        the user has for each.
+        """
+
+        lfm_period = PERIODS[period]
+
+        user: pylast.User = self.network.get_user(name)
+        fav_artists: list[pylast.TopItem] = user.get_top_artists(
+            period=lfm_period, limit=10
+        )
+
+        artists_str: str = str()
+        for i in range(10):
+            artists_str += f"\n{i+1}) [{fav_artists[i].item.name}]({fav_artists[i].item.get_url()}) - **{fav_artists[i].weight}** scrobbles"
+
+        embed = discord.Embed(
+            title=f"{user.get_name()}'s Top 10 Artists ({period})",
+            color=discord.Color.gold(),
+            description=artists_str,
+        )
+
+        embed.set_thumbnail(url=ctx.user.avatar.url)
+
+        await ctx.respond(embed=embed)
 
 
 def setup(bot: discord.Bot):
