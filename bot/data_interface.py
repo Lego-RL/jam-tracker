4980932e0be5d60e223b53b5ccc83c9f99811a79
Lego-RL <joeylbarroso@gmail.com>
2022-06-24 18:12:53 -0500

    migrate from json to sqlalchemy db

--- bot/data_interface.py
+++ bot/data_interface.py
@@ -1,25 +1,80 @@
 import discord
 
-import json
-import platform
+import sqlalchemy
+from sqlalchemy import Column, ForeignKey, DateTime, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
-up_one_file = ".." if platform.platform().lower().find("linux") > -1 else "."
-data_path = up_one_file + "/data/lfm-users.json"
+import datetime
+
+import os
+from os import pardir
+
+db_path = os.path.join("data", "user_scrobble_data.db")
+
+engine = create_engine(url=f"sqlite:///{db_path}", future=True)
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session: Session = Session()
 
 
-def store_user(discord_id: int, lfm_user: str) -> None:
+class User(Base):
+    __tablename__ = "user_account"
+
+    id = Column(Integer, primary_key=True)
+    discord_id = Column(Integer, nullable=False)
+    last_fm_user = Column(String, nullable=False)
+
+    # each user can have many scrobble entries
+    scrobble_entries = relationship("Scrobbles", back_populates="user")
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, discord_id={self.discord_id!r}, last_fm_user={self.last_fm_user!r})"
+
+
+class Scrobbles(Base):
+    __tablename__ = "scrobbles"
+
+    id = Column(Integer, primary_key=True)
+
+    title = Column(String, nullable=False)
+    artist = Column(String, nullable=False)
+    album = Column(String)
+
+    # in seconds
+    listen_duration = Column(Integer, nullable=False)
+    date = Column(DateTime, nullable=False)
+
+    user_id = Column(Integer, ForeignKey("user_account.id"))
+
+    # each scrobble belongs to one single user
+    # user = relationship("User", back_populates="scrobble_entries", uselist=False)
+    user = relationship("User", uselist=False)
+
+
+Base.metadata.create_all(engine)
+
+
+def store_user(discord_id: int, lfm_user: str) -> bool:
     """
-    Store pairing of discord user id & their associated
-    last.fm account in lfm-users.json.
+    Adds user to user_account table and returns
+    True if successful, otherwise False if
+    user already had their info stored.
     """
 
-    with open(data_path, "r") as f:
-        data: dict[str, str] = json.load(f)
+    with Session.begin() as session:
+        exists = (
+            session.query(User.discord_id).filter_by(discord_id=discord_id).first()
+            is not None
+        )
 
-    data[str(discord_id)] = lfm_user
+        if exists:
+            return False
 
-    with open(data_path, "w") as f:
-        json.dump(data, f, indent=4)
+        new_user = User(discord_id=discord_id, last_fm_user=lfm_user)
+        session.add(new_user)
+
+        return True
 
 
 def retrieve_lfm_username(discord_id: int) -> str:
@@ -28,12 +83,15 @@ def retrieve_lfm_username(discord_id: int) -> str:
     user id.
     """
 
-    with open(data_path, "r") as f:
-        data: dict = json.load(f)
+    with Session.begin() as session:
+        user: str = (
+            session.query(User.last_fm_user).filter_by(discord_id=discord_id).scalar()
+        )
 
-    lfm_user = data.get(str(discord_id))
+        if user:
+            return user
 
-    return lfm_user if lfm_user else None
+        return None  # user not found
 
 
 def get_correct_lfm_user(invoker_id: int, user: discord.User) -> str:
